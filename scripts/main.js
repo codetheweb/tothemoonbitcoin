a4e10e9c53113008f0e215db4e38bf6bb679e47d
Max Isom <codetheweb@icloud.com>
2017-06-05 12:17:14 -0400

    Added custom measure type & Github star button

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,45 @@ $(document).ready(function() {
 
   updateMeasure(238855);
 
-  // Button events
+  /*-- Button events --*/
   $('.measure-number').click(function(event) {
     $(this).addClass('active'); // add active effect on click
     $('.measure-number').not(this).removeClass('active'); // remove active effect on other options
-    updateMeasure($(this).data('measure'));
+
+    if ($(this).attr('type') != 'text') { // don't call updateMeasure on #measure-custom
+      updateMeasure($(this).data('measure'));
+    }
+    else {
+      showSpinner(true);
+    }
   });
 
+  /*-- Custom measure event --*/
+  // So that the page doesn't fire a HTTP request
+  // after every keystroke, we wait for 1 seconds
+  // after no typing has been detected before
+  // sending anything. https://stackoverflow.com/a/5926782.
+  var typingTimer;
+
+  $('#measure-custom').keyup(function(event) {
+    clearTimeout(typingTimer);
+
+    if ($(this).val()) {
+      typingTimer = setTimeout(customMeasureUpdate, 1000);
+    }
+  });
+
+  // Is called after 2 seconds of no typing in
+  // the custom measure input.
+  function customMeasureUpdate() {
+    // Extracts numbers, works with comma-seperated digits
+    // and decimals.
+    var numbers = $('#measure-custom').val().replace(',', '').match(/[-]{0,1}[\d.]*[\d]+/g);
+    updateMeasure(numbers[0]);
+  }
+
   function updateMeasure(measure) {
-    $('#price').hide();
-    $('.spinner').show();
+    showSpinner(true);
 
     getPriceUSD(function(price) {
       $('#price').html('(1Ƀ = $' + price + ')');
@@ -20,9 +49,13 @@ $(document).ready(function() {
       var percentRounded = ((price/measure) * 100).toFixed(2);
       $('#percentage > .number').html(percentRounded + '%');
 
-      $('#price').show();
-      $('.spinner').hide();
+      showSpinner(false);
     });
+  }
+
+  function showSpinner(toggle) {
+    $('#price').toggle(!toggle);
+    $('.spinner').toggle(toggle);
   }
 
   // gets current USD price of 1 bitcoin from CoinDesk
